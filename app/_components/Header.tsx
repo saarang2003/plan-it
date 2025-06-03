60dae63dbdc6770c9cd6cff597322f7d5e7c4ba2
sarang <saranggp2018@gmail.com>
2025-06-03 12:02:15 +0530

    completed hero component

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -78,14 +78,14 @@ function Header() {
             <div className="flex items-center gap-4">
               <div className="sm:flex sm:gap-4">
                 <a
-                  className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
+                  className="block rounded-md px-5 py-2.5 text-sm font-medium text-white transition "
                   href="#"
                 >
                   Login
                 </a>
 
                 <a
-                  className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
+                  className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-black transition hover:text-black-600/75 sm:block"
                   href="#"
                 >
                   Register
